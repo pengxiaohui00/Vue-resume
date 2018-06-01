8408ea919758007394acf43e4e41e0cbf9b5baba
pengronghui00 <1341660796@qq.com>
2018-06-02 00:27:11 +0800

    页面分为三部分

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+   <Topbar class="topbar"/>
+  <main>
+     <Editor class="editor"/>
+   <Preview class="preview"/>
+  </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Topbar from './components/Topbar'
+import Editor from './components/Editor'
+import Preview from './components/Preview'
+
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    Topbar,Editor,Preview
   }
 }
 </script>
 
-<style>
+<style lang='scss'>
+html,body,#app{
+  height:100%;
+  overflow: hidden;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
 }
+.topbar{
+  background-color: #ccc;
+}
+main{
+  background-color:#ddd;
+  display: flex;
+  flex:1;
+   >.editor {
+   width: 20em;
+}
+ >.preview{
+  flex: 1;
+}
+
+}
+
+
 </style>
